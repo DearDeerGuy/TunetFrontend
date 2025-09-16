7648fced5d526bf3e3d31535f28a00512f8d4d5e
Aist023 <lololo228505aist@gmail.com>
2025-09-16 23:07:00 +0300

    added side menu to heder

--- src/router/ListRouter.jsx
+++ src/router/ListRouter.jsx
@@ -27,7 +27,7 @@ export const ListAuthorizedUser = [
     {id:4,path:"/adminPanel",element:<Form><AdminPanelPage/></Form>},
     {id:5,path:"/addMovie",element:<Form><AddChangeMovie/></Form>},
     {id:6,path:"/changeMovie/:id",element:<Form><AddChangeMovie/></Form>},
-    {id:7,path:"/gallery",element:<Form><GalleryPage/></Form>},
+    {id:7,path:"/gallery",element:<Form><GalleryPage key="gallery"/></Form>},
     {id:8,path:"/view/:id",element:<Form><ViewPage/></Form>},
-    {id:9,path:"/favorite",element:<Form><GalleryPage/></Form>},
+    {id:9,path:"/favorite",element:<Form><GalleryPage key="favorite"/></Form>},
 ]
